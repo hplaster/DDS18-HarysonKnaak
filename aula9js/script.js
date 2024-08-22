81ff6510dc94a1f2f47361fd9f30574446e51585
Haryson Knaak <haryson.plaster@aluno.senai.br>
2024-08-22 16:54:55 -0300

    Atualizando Aula 9

--- aula9js/script.js
+++ aula9js/script.js
@@ -1,12 +1,142 @@
-//Cria um alerta na tela
-alert("TESTE")
+// alert("Outra mensagem");
 
-//CRIA UMA MENSAGEM NO CONSOLE DO NAVEGADOR
-console.log("OLHA O TEXTO")
+// console.log("Olha eu dnv");
 
-var texto = "Eae, fml "
-var numero = 1989
-var theBest = true
-var nulo = null
+// var text = "Gabryel";
+// var number = 1955;
+// var theBest = true;
+// var nulo = null;
+// var indefinida;
 
-console.log(texto, numero, theBest, nulo)
+
+// console.log(typeof(text));
+// console.log(typeof(number));
+// console.log(typeof(theBest));
+// console.log(typeof(nulo));
+// console.log(typeof(indefinida));
+
+//=======================================================
+
+// ARITMÉTICOS
+// var a = 10
+// var b = 3
+
+// console.log("Soma -", a+b);
+// console.log("Subtração -", a-b);
+// console.log("Multiplicação -", a*b);
+// console.log("Divisão - ", a/b);
+// console.log("Módulo -", a%b);
+
+//========================================================
+
+//LÓGICOS
+// var verdade = true;
+// var mentira = false;
+
+// console.log(verdade && mentira);
+// console.log(verdade || mentira);
+// console.log(verdade && !mentira);
+
+//=======================================================
+
+// //Relacionais;
+// var a = 10;
+// var b = 5;
+// var c = "10";
+
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a >= b);
+// console.log(a <= b);
+// console.log(a == b);
+// console.log(a != b);
+// console.log(a == c);
+// console.log(a === c);
+// console.log(a !== c);
+
+//=====================================================
+
+//Condicionais
+
+// var estaVivo = true
+// if (estaVivo) {
+//     console.log("Está vivo")
+// } else if (!estaVivo) {
+//     console.log("Está morto");
+// } else {
+//     console.log("Não tá nada");
+// }
+
+//=====================================================
+
+//Switch 
+// var camisa = "Preta";
+
+// switch (camisa) {
+//     case "Azul":
+//         console.log("A camisa é Azul!");
+//         break;
+//     case "Amarela":
+//         console.log("A camisa é Amarela");
+//         break
+//     case "Preta":
+//         console.log("A camisa é Preta");
+//         break;
+//     default:
+//         console.log("Animal");
+//         break;
+
+// };
+
+//=====================================================
+
+//Loops
+
+// for(var i = 0; i < 5
+//     ; i++){
+//     console.log("Estoy ahi!");
+// }
+// console.log("Acabou o FOR");
+
+//=====================================================
+
+// var it = 1
+// while(it < 10){
+//     console.log("Pupapaparatata");
+//     it++
+// };
+
+//=====================================================
+
+// function teste(){
+//     console.log("Funcao 1");
+// }
+
+
+// function teste2(){
+//     console.log("Funcao 2");
+// }
+
+// teste()
+// teste2()
+
+//=====================================================
+
+// var resposta = prompt("Digite um algo:");
+// var resposta2 = prompt("Digite um outro algo:");
+
+// console.log(resposta, resposta2);
+
+// alert(resposta + " " + resposta2);
+
+//=====================================================
+
+
+// ATIVIDADE
+// var N1 = Number(prompt("Informe um número: "));
+// var N2 = Number(prompt("Informe outro número: "));
+// var soma = N1 + N2 
+
+// alert(soma)
+
+// console.log(soma)
